22ea56b380181a5b3fa1ff6abb4fc6fed190d5bf
Alex Griffen <radhack242@gmail.com>
2017-06-02 15:27:43 -0700

    update signerpage.php for bug testing

--- signerpage.php
+++ signerpage.php
@@ -22,7 +22,7 @@
                             window.location = "index.php";
                         } else if (eventData.event == HelloSign.EVENT_CANCELED) {
                             HelloSign.close();
-                            alert("Event Canceled And Stuff!");
+//                            alert("Event Canceled And Stuff!");
                             console.log(eventData);
                             window.location = "index.php";
                         } else if (eventData.event == HelloSign.EVENT_ERROR) {
@@ -32,18 +32,20 @@
                             window.location = "index.php";
                         } else if (eventData.event == HelloSign.EVENT_SENT) {
                             HelloSign.close();
-                            alert("Signature Request Sent And Stuff!");
+//                            alert("Signature Request Sent And Stuff!");
                             console.log(eventData);
+                            console.log("************");
+                            console.log(eventData.signature_request_id);
                             window.location = "index.php";
                         } else if (eventData.event == HelloSign.EVENT_TEMPLATE_CREATED) {
                             HelloSign.close();
                             var template_id = eventData.template_id;
-                            window.alert("Template ID is " + template_id);
+//                            window.alert("Template ID is " + template_id);
                             console.log(eventData);
                             window.location = "index.php";
                         } else if (eventData.event == HelloSign.EVENT_DECLINED) {
                             HelloSign.close();
-                            alert("Signature Request declined And Stuff!");
+//                            alert("Signature Request declined And Stuff!");
                             console.log(eventData);
                             window.location = "index.php";
                         }
