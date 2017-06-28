3c406089aeaf278abc89e17b8270e8db33b3ea0b
Alex Griffen <radhack242@gmail.com>
2017-06-28 11:54:27 -0700

    update auth.php to disable LaunchDarkly

--- auth.php
+++ auth.php
@@ -9,4 +9,4 @@ $client_id = getenv('HS_CLIENT_ID_LOCAL') ? getenv('HS_CLIENT_ID_LOCAL') : '';
 //$client_id = 'eba963c9302dbc5c91d07ac3fe100c18';
 $sendgrid_api_key = getenv('SENDGRID_PHP_APIKEY') ? getenv('SENDGRID_PHP_APIKEY') : '';
 //$ld_client = getenv('ld_client') ? getenv('ld_client') : '';
-$ld_client = new LaunchDarkly\LDClient(getenv('ld_client'));
+//$ld_client = new LaunchDarkly\LDClient(getenv('ld_client'));
