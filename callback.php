349d6d69065cc4a47ab8966f2cc4dc09ce0fe8ef
Alex Griffen <radhack242@gmail.com>
2017-06-28 11:54:05 -0700

    update callback.php to stop notifications

--- callback.php
+++ callback.php
@@ -133,7 +133,6 @@
 //    echo ("*****HASH CHECK<br />$hash_check<br />");
 
     $callback_event = new HelloSign\Event($data);
-    print_r($callback_event);
     if ($callback_event->isValid($api_key) == FALSE) {
         $hash_check_failed = 1;
         goto invalid_hash;
@@ -155,14 +154,14 @@
     // request is completely signed by all signees, HelloSign has processed
     // the document and has it available for download.
     // if ($reported_app === 'afedad951b68dc42bfbd930e81d97175') {
-    if ($reported_app === '3e3f283135002d1993a92124341193df') {
-//    if ($reported_app === 'xxx') { //stop processing callbacks for now
+//    if ($reported_app === '3e3f283135002d1993a92124341193df') {
+    if ($reported_app === 'xxx') { //stop processing callbacks for now
         if ($event_type === 'signature_request_all_signed') {
             $client = new HelloSign\Client($api_key);
             $signature_request_id = $data->signature_request->signature_request_id;
-            $requester = $data->siganture_request->requester_email_address;
-            $signer_email = $data->signature_request->sigantures->signer_email_address;
-            $signer_name = $data->signature_request->sigantures->signer_name;
+            $requester = $data->signature_request->requester_email_address;
+            $signer_email = $data->signature_request->signatures->signer_email_address;
+            $signer_name = $data->signature_request->signatures->signer_name;
 
             // Here you define where the file should download to. This should be
             // customized to your app's needs.
@@ -269,7 +268,7 @@
             $sql = "UPDATE signatureId SET event_sent_bool='1' WHERE signature_request_id='$signature_request_id'";
 
 //            if ($conn->query($sql) === TRUE) {
-            if ($conn->query($sql) == "DERP") {
+            if ($conn->query($sql) == FALSE) {
                 $event_time = $data->event->event_time;
                 $sendgrid = new SendGrid($sendgrid_api_key);
                 $url = 'https://api.sendgrid.com/';
@@ -306,42 +305,42 @@
 // print everything out
                 print_r($response);
             } else {
-
-                $event_time = $data->event->event_time;
-                $sendgrid = new SendGrid($sendgrid_api_key);
-                $url = 'https://api.sendgrid.com/';
-                $pass = $sendgrid_api_key;
-
-                $params = array(
-                    'to' => "radhack242+$event_type@gmail.com",
-                    'toname' => "Signature Request Sent",
-                    'from' => "radhack242@gmail.com",
-                    'fromname' => "Simple PHP",
-                    'subject' => "$event_type received",
-                    'html' => "<strong>$signature_request_id</strong> Error INSERTing (lol): $conn->error <br />Is the signature_request_id<br />$event_type was received at $event_time<br />",
-                );
-
-                $request = $url . 'api/mail.send.json';
-
-// Generate curl request
-                $session = curl_init($request);
-// Tell PHP not to use SSLv3 (instead opting for TLS)
-                curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-                curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $sendgrid_api_key));
-// Tell curl to use HTTP POST
-                curl_setopt($session, CURLOPT_POST, true);
-// Tell curl that this is the body of the POST
-                curl_setopt($session, CURLOPT_POSTFIELDS, $params);
-// Tell curl not to return headers, but do return the response
-                curl_setopt($session, CURLOPT_HEADER, false);
-                curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
-// obtain response
-                $response = curl_exec($session);
-                curl_close($session);
-
-// print everything out
-                print_r($response);
+                echo "*_?>*_?>*_?>*_?> skipping <br />";
+//                $event_time = $data->event->event_time;
+//                $sendgrid = new SendGrid($sendgrid_api_key);
+//                $url = 'https://api.sendgrid.com/';
+//                $pass = $sendgrid_api_key;
+//
+//                $params = array(
+//                    'to' => "radhack242+$event_type@gmail.com",
+//                    'toname' => "Signature Request Sent",
+//                    'from' => "radhack242@gmail.com",
+//                    'fromname' => "Simple PHP",
+//                    'subject' => "$event_type received",
+//                    'html' => "<strong>$signature_request_id</strong> Error INSERTing (lol): $conn->error <br />Is the signature_request_id<br />$event_type was received at $event_time<br />",
+//                );
+//
+//                $request = $url . 'api/mail.send.json';
+//
+//// Generate curl request
+//                $session = curl_init($request);
+//// Tell PHP not to use SSLv3 (instead opting for TLS)
+//                curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+//                curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $sendgrid_api_key));
+//// Tell curl to use HTTP POST
+//                curl_setopt($session, CURLOPT_POST, true);
+//// Tell curl that this is the body of the POST
+//                curl_setopt($session, CURLOPT_POSTFIELDS, $params);
+//// Tell curl not to return headers, but do return the response
+//                curl_setopt($session, CURLOPT_HEADER, false);
+//                curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+//
+//// obtain response
+//                $response = curl_exec($session);
+//                curl_close($session);
+//
+//// print everything out
+//                print_r($response);
             }
             $conn->close();
         } elseif ($event_type === 'file_error') {
