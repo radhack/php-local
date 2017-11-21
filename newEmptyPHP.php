671c51caec19e4a55c28ea66e0195fe599f4c0ff
Alex Griffen <radhack242@gmail.com>
2017-11-21 15:38:05 -0800

    add db.php to call loop to download signature requests

--- newEmptyPHP.php
+++ newEmptyPHP.php
@@ -8,12 +8,8 @@
         require_once 'vendor/autoload.php';
         include('auth.php');
 
-        $client = new HelloSign\Client($api_key);
-
-        $sig_req_response = $client->getFiles("6adc00d19d91679ec47c79e4ed180fd33121123b", '', "link");
-        $file_url = $sig_req_response->file_url;
-
-        echo("<br />");
-//        print_r($file_url);
-//echo "$file_url";
-        ?><iframe src="<?php echo "$file_url" ?>" height="900" width="800"></iframe></body></html>
+        $is_loop_time = 1;
+        include('db.php');
+        ?>
+    </body>
+</html>
