5f1f6e6c9384a4eeeaa1069682beee6bf0428377
Alex Griffen <radhack242@gmail.com>
2018-01-22 14:32:59 -0800

    delete all tests and reset

--- newEmptyPHP.php
+++ newEmptyPHP.php
@@ -1,15 +1,2 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>New Empty PHP</title>
-    </head>
-    <body>
-        <?php
-        require_once 'vendor/autoload.php';
-        include('auth.php');
+<?php
 
-        $is_loop_time = 1;
-        include('db.php');
-        ?>
-    </body>
-</html>
