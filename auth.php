82f565f3159a94faae5f3f82d65ed7b237a11f47
Alex Griffen <radhack242@gmail.com>
2018-01-28 11:07:20 -0800

    update with new auth options

--- auth.php
+++ auth.php
@@ -11,6 +11,7 @@ if ($hfapi == 1) {
     $client_id = getenv('HFAPI_HS_CLIENT_ID') ? getenv('HFAPI_HS_CLIENT_ID') : '';
 } else {
     $api_key = getenv('HS_APIKEY_PROD') ? getenv('HS_APIKEY_PROD') : '';
+    $api_key_1 = getenv('HS_APIKEY_MY_PROD') ? getenv('HS_APIKEY_MY_PROD') : '';
     $client_id = getenv('HS_CLIENT_ID_LOCAL') ? getenv('HS_CLIENT_ID_LOCAL') : '';
 //    $client_id = 'eba963c9302dbc5c91d07ac3fe100c18';
 //    $client_id = 'd4b5e20504531509d49e31c5f5bfae62';
