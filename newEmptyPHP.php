ba956fa6123d0206d23d4c338f6e5bd1efb18ece
Alex Griffen <radhack242@gmail.com>
2018-02-09 07:32:43 -0800

    update newEmpty

--- newEmptyPHP.php
+++ newEmptyPHP.php
@@ -1,2 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>New Empty</title>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+        <script type="text/javascript" src="//s3.amazonaws.com/cdn.hellosign.com/public/js/hellosign-embedded.LATEST.min.js"></script>
+        <!--<script type="text/javascript" src="//s3.amazonaws.com/cdn.hellofax.com/js/embedded.js"></script>-->
+        <link rel="stylesheet" type="text/css" href="newcss.css" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg"/>
+    </head>
+    <body>
+        <?php
 
+        ?>
+    </body>
+</html>
